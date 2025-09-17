992d1b5a67eadb8ea1449114db99547a7f6ee661
Sreenath <sreenathsrp256@gmail.com>
2025-09-17 16:16:53 +0530

    First Commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+
+import { Route, Routes } from 'react-router-dom'
+import Login from './pages/Login'
+
+function App() {
+
+  return (
+    <div className="App">
+      <Routes>
+        <Route path="/" element={<Login />} />
+      </Routes>
+
+    </div>
+  )
+}
+
+export default App
---
+++ src/pages/Login.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Login = () => {
+  return (
+    <div className='text-red-500'>
+      Login Page
+    </div>
+  )
+}
+
+export default Login
